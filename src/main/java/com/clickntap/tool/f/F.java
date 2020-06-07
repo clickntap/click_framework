e11a5fdb206050d31cd71a2cbeb1cb0b1302d480
Tonino Mendicino <tonino@clickntap.com>
2020-06-07 19:23:19 +0200

    Update F.java

--- src/main/java/com/clickntap/tool/f/F.java
+++ src/main/java/com/clickntap/tool/f/F.java
@@ -10,56 +10,66 @@ import com.clickntap.api.CryptoUtils;
 
 public class F {
 
-  private ScriptEngine javascriptEngine;
-  private Resource file;
-  private CryptoUtils crypto;
+	private ScriptEngine javascriptEngine;
+	private Resource file;
+	private CryptoUtils crypto;
+	private String serverURL;
 
-  public Resource getFile() {
-    return file;
-  }
+	public String getServerURL() {
+		return serverURL;
+	}
 
-  public void setFile(Resource file) {
-    this.file = file;
-  }
+	public void setServerURL(String serverURL) {
+		this.serverURL = serverURL;
+	}
 
-  public CryptoUtils getCrypto() {
-    return crypto;
-  }
+	public Resource getFile() {
+		return file;
+	}
 
-  public void setCrypto(CryptoUtils crypto) {
-    this.crypto = crypto;
-  }
+	public void setFile(Resource file) {
+		this.file = file;
+	}
 
-  public void init() throws Exception {
-    ScriptEngineManager manager = new ScriptEngineManager();
-    javascriptEngine = manager.getEngineByName("nashorn");
-    javascriptEngine.put("util", new Util(file, crypto));
-    javascriptEngine.eval("var Proxy = function(){};");
-    javascriptEngine.eval("var document = {};");
-    javascriptEngine.eval("var console = {};");
-    javascriptEngine.eval("var window = {};");
-    javascriptEngine.eval("var sessionStorage = {};");
-    javascriptEngine.eval("document.addEventListener = function() {};");
-    javascriptEngine.eval("console.log = print;");
-    javascriptEngine.eval("window.addEventListener = function() {};");
-    javascriptEngine.eval("sessionStorage.getItem = function() { return null; };");
-    load();
-  }
+	public CryptoUtils getCrypto() {
+		return crypto;
+	}
 
-  public void load() throws Exception {
-    javascriptEngine.eval("load('" + file.getFile().getAbsolutePath() + "');");
-  }
+	public void setCrypto(CryptoUtils crypto) {
+		this.crypto = crypto;
+	}
 
-  public Object run(String script) throws Exception {
-    return javascriptEngine.eval(script);
-  }
+	public void init() throws Exception {
+		ScriptEngineManager manager = new ScriptEngineManager();
+		javascriptEngine = manager.getEngineByName("nashorn");
+		javascriptEngine.put("util", new Util(file, crypto));
+		javascriptEngine.eval("var Proxy = function(){};");
+		javascriptEngine.eval("var document = {};");
+		javascriptEngine.eval("var console = {};");
+		javascriptEngine.eval("var window = {};");
+		javascriptEngine.eval("var sessionStorage = {};");
+		javascriptEngine.eval("document.addEventListener = function() {};");
+		javascriptEngine.eval("console.log = print;");
+		javascriptEngine.eval("window.addEventListener = function() {};");
+		javascriptEngine.eval("sessionStorage.getItem = function() { return null; };");
+		load();
+	}
 
-  public String chart(JSONObject json) throws Exception {
-    return run("f().chart(" + json.toString() + ").render()").toString();
-  }
+	public void load() throws Exception {
+		javascriptEngine.eval("load('" + file.getFile().getAbsolutePath() + "');");
+		javascriptEngine.eval("f().serverURL('" + getServerURL() + "')");
+	}
 
-  public ScriptEngine getJavascriptEngine() {
-    return javascriptEngine;
-  }
+	public Object run(String script) throws Exception {
+		return javascriptEngine.eval(script);
+	}
+
+	public String chart(JSONObject json) throws Exception {
+		return run("f().chart(" + json.toString() + ").render()").toString();
+	}
+
+	public ScriptEngine getJavascriptEngine() {
+		return javascriptEngine;
+	}
 
 }
