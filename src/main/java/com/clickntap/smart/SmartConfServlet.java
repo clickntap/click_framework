cca86b89c570bb24733d99cc5a630fdff0607599
Tonino Mendicino <tonino@clickntap.com>
2020-08-21 13:34:59 +0200

    Update SmartConfServlet.java

--- src/main/java/com/clickntap/smart/SmartConfServlet.java
+++ src/main/java/com/clickntap/smart/SmartConfServlet.java
@@ -19,95 +19,95 @@ import com.clickntap.utils.ConstUtils;
 
 public class SmartConfServlet implements Servlet {
 
-	public static boolean checkEnvPath(String path, String env) {
-		try {
-			int x = path.indexOf(env);
-			if (!Character.isLetterOrDigit(path.charAt(x - 1)) && !Character.isLetterOrDigit(path.charAt(x + env.length()))) {
-				return true;
-			}
-		} catch (Exception e) {
-		}
-		return false;
-	}
+  public static boolean checkEnvPath(String path, String env) {
+    try {
+      int x = path.indexOf(env);
+      if (!Character.isLetterOrDigit(path.charAt(x - 1)) && !Character.isLetterOrDigit(path.charAt(x + env.length()))) {
+        return true;
+      }
+    } catch (Exception e) {
+    }
+    return false;
+  }
 
-	public void init(ServletConfig conf) throws ServletException {
-		try {
-			File root = new File(conf.getServletContext().getRealPath(""));
-			File webInf = new File(conf.getServletContext().getRealPath("WEB-INF"));
-			File dir = new File(webInf.getCanonicalPath() + getPathSeparator() + "conf");
-			File out = new File(dir.getCanonicalPath() + getPathSeparator() + "auto");
-			out.mkdirs();
-			Properties envProperties = new Properties();
-			envProperties.putAll(System.getProperties());
-			String envFile = conf.getInitParameter("smartEnv");
-			int x = envFile.lastIndexOf('/');
-			if (x >= 0) {
-				envFile = envFile.substring(x + 1);
-			}
-			if (envFile != null) {
-				File etcParentDir = root;
-				while (etcParentDir != null) {
-					File file = new File(etcParentDir.getCanonicalPath() + "/etc/" + envFile);
-					if (file.exists()) {
-						envFile = file.getCanonicalPath();
-						break;
-					}
-					etcParentDir = etcParentDir.getParentFile();
-				}
-				File file = new File(envFile);
-				if (file.exists()) {
-					FileInputStream in = new FileInputStream(file);
-					envProperties.load(in);
-					in.close();
-					file = new File(file.getCanonicalPath().replace("-env", "-log4j"));
-					if (file.exists()) {
-						PropertyConfigurator.configure(file.getCanonicalPath().replace("-env", "-log4j"));
-					} else {
-						System.out.println("LOG4J NOT FOUND: " + file.getCanonicalPath().replace("-env", "-log4j"));
-					}
-				} else {
-					System.out.println("ENV NOT FOUND: " + file.getCanonicalPath());
-				}
-			}
-			FileUtils.cleanDirectory(out);
-			if (dir.exists() && dir.isDirectory()) {
-				for (File file : dir.listFiles()) {
-					if (file.isFile()) {
-						String data = FileUtils.readFileToString(file, ConstUtils.UTF_8);
-						Enumeration enu = envProperties.keys();
-						while (enu.hasMoreElements()) {
-							String key = enu.nextElement().toString();
-							data = data.replaceAll("\\[" + key + "\\]", envProperties.getProperty(key));
-						}
-						data = data.replaceAll("\\[WEB-INF\\]", webInf.getCanonicalPath().replace("\\", "/"));
-						data = data.replaceAll("\\[ROOT\\]", root.getCanonicalPath().replace("\\", "/"));
-						out.mkdirs();
-						FileUtils.writeStringToFile(new File(out.getAbsolutePath() + getPathSeparator() + file.getName()), data, ConstUtils.UTF_8);
-					}
-				}
-			}
-		} catch (Exception e) {
-			throw new ServletException(e);
-		}
-	}
+  public void init(ServletConfig conf) throws ServletException {
+    try {
+      File root = new File(conf.getServletContext().getRealPath(""));
+      File webInf = new File(conf.getServletContext().getRealPath("WEB-INF"));
+      File dir = new File(webInf.getCanonicalPath() + getPathSeparator() + "conf");
+      File out = new File(dir.getCanonicalPath() + getPathSeparator() + "auto");
+      out.mkdirs();
+      Properties envProperties = new Properties();
+      envProperties.putAll(System.getProperties());
+      String envFile = conf.getInitParameter("smartEnv");
+      if (envFile != null) {
+        int x = envFile.lastIndexOf('/');
+        if (x >= 0) {
+          envFile = envFile.substring(x + 1);
+        }
+        File etcParentDir = root;
+        while (etcParentDir != null) {
+          File file = new File(etcParentDir.getCanonicalPath() + "/etc/" + envFile);
+          if (file.exists()) {
+            envFile = file.getCanonicalPath();
+            break;
+          }
+          etcParentDir = etcParentDir.getParentFile();
+        }
+        File file = new File(envFile);
+        if (file.exists()) {
+          FileInputStream in = new FileInputStream(file);
+          envProperties.load(in);
+          in.close();
+          file = new File(file.getCanonicalPath().replace("-env", "-log4j"));
+          if (file.exists()) {
+            PropertyConfigurator.configure(file.getCanonicalPath().replace("-env", "-log4j"));
+          } else {
+            System.out.println("LOG4J NOT FOUND: " + file.getCanonicalPath().replace("-env", "-log4j"));
+          }
+        } else {
+          System.out.println("ENV NOT FOUND: " + file.getCanonicalPath());
+        }
+      }
+      FileUtils.cleanDirectory(out);
+      if (dir.exists() && dir.isDirectory()) {
+        for (File file : dir.listFiles()) {
+          if (file.isFile()) {
+            String data = FileUtils.readFileToString(file, ConstUtils.UTF_8);
+            Enumeration enu = envProperties.keys();
+            while (enu.hasMoreElements()) {
+              String key = enu.nextElement().toString();
+              data = data.replaceAll("\\[" + key + "\\]", envProperties.getProperty(key));
+            }
+            data = data.replaceAll("\\[WEB-INF\\]", webInf.getCanonicalPath().replace("\\", "/"));
+            data = data.replaceAll("\\[ROOT\\]", root.getCanonicalPath().replace("\\", "/"));
+            out.mkdirs();
+            FileUtils.writeStringToFile(new File(out.getAbsolutePath() + getPathSeparator() + file.getName()), data, ConstUtils.UTF_8);
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new ServletException(e);
+    }
+  }
 
-	private String getPathSeparator() {
-		return System.getProperty("file.separator");
-	}
+  private String getPathSeparator() {
+    return System.getProperty("file.separator");
+  }
 
-	public void destroy() {
-	}
+  public void destroy() {
+  }
 
-	public ServletConfig getServletConfig() {
-		return null;
-	}
+  public ServletConfig getServletConfig() {
+    return null;
+  }
 
-	public String getServletInfo() {
-		return null;
-	}
+  public String getServletInfo() {
+    return null;
+  }
 
-	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+  public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
 
-	}
+  }
 
 }
